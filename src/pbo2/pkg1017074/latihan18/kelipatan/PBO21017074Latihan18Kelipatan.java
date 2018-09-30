020ba5f47e1d48bf6f127e7d97af5c75ceacd264
MY PC <MY PC@HOME-UEFI1TB.local>
2018-10-01 00:18:28 +0700

    Project Brigita Julia 10117074 PBO2

---
+++ src/pbo2/pkg1017074/latihan18/kelipatan/PBO21017074Latihan18Kelipatan.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg1017074.latihan18.kelipatan;
+
+/**
+ *
+ * @author Rizki Adam Kurniawan
+ */
+public class PBO21017074Latihan18Kelipatan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("For Ascending");
+        for (double i = 3.5; i <= 35; i += 3.5){
+            System.out.println(i);
+    }
+    
+}
